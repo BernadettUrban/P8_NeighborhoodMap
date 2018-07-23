4e8507d45205f801121f828989ad9bbe619587dd
Bernadett Urbán <bittibettibott@windowslive.com>
2018-07-23 08:58:51 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Neighborhood Map</h1>
-        </header>
-          <div>
-            Neighborhood Map
-          </div>
+      <div>
+        Neighborhood Map
       </div>
     );
   }
