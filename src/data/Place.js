1816296938fcc6f084e9fac634a9e78d78df23c6
Bernadett Urbán <bittibettibott@windowslive.com>
2018-07-23 09:37:48 +0200

    Create Place.js
    place component

---
+++ src/data/Place.js
@@ -0,0 +1,11 @@
+export class Place {
+    constructor(key, title, lat, lng, type) {
+        this.key = key;
+        this.title = title;
+        this.lat = lat;
+        this.lng = lng;
+        this.type = type;
+    }
+}
+
+export default Place;
