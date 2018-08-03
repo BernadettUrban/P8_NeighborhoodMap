275c0bc327aaea184e5c98536a688946c788d235
Bernadett Urbán <bittibettibott@windowslive.com>
2018-08-03 09:04:57 +0200

    feat: changing the model

--- src/data/Data.js
+++ src/data/Data.js
@@ -1,34 +1,22 @@
-import { Place } from './Place';
+import { Place } from './Models';
+import { PlaceCategory } from './Models';
+
+const Restaurant = new PlaceCategory('restaurant', 'Restaurants', 'fa-utensils');
+const Mall = new PlaceCategory('mall', 'Malls', 'fa-shopping-cart');
+const Sight = new PlaceCategory('sight', 'Sights', 'fa-camera-retro');
+const Business = new PlaceCategory('business', 'Business', 'fa-building');
 
 export const PLACES = [
-    new Place('1', 'Hold utcai piac',47.504828 , 19.052649, 'restaurant', '4bd02be477b29c74233a8a82'),
-    new Place('2', 'WestEnd City Center', 47.513452, 19.059390, 'mall', '4bcc1b343740b71309846365'),
-    new Place('3', 'Parlament', 47.507476, 19.046693, 'sight', '4bd6e74b5631c9b63889a630'),
-    new Place('4', 'Toldi Mozi', 47.503835, 19.054623, 'business', '4b733f2af964a52018a42de3'),
-    new Place('5', 'Mazi Greek Kitchen', 47.507391, 19.052537, 'restaurant', '574f05ee498e831452b8f54d'),
-    
-    //TODO add places new Place(),
+    new Place('1', 'Hold utcai piac',47.504828 , 19.052649, 'Restaurant', '4bd02be477b29c74233a8a82'),
+    new Place('2', 'WestEnd City Center', 47.513452, 19.059390, 'Mall', '4bcc1b343740b71309846365'),
+    new Place('3', 'Parlament', 47.507476, 19.046693, 'Sight', '4bd6e74b5631c9b63889a630'),
+    new Place('4', 'Toldi Mozi', 47.503835, 19.054623, 'Business', '4b733f2af964a52018a42de3'),
+    new Place('5', 'Mazi Greek Kitchen', 47.507391, 19.052537, 'Restaurant', '574f05ee498e831452b8f54d'),
 ];
 
-export const PlaceCategories = [
-    {
-        id: 'restaurant',
-        icon: 'fa-utensils',
-        title: 'Restaurant'
-    },
-    {
-        id: 'mall',
-        icon: 'fa-shopping-cart',
-        title: 'Mall'
-    },
-    {
-        id: 'sight',
-        icon: 'fa-camera-retro',
-        title: 'Sights'
-    },
-    {
-        id: 'business',
-        icon: 'fa-building',
-        title: 'Business'
-    }
+export const CATEGORIES = [
+    Restaurant,
+    Mall,
+    Sight,
+    Business
 ];
